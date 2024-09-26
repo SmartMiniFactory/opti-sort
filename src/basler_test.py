c2dd52c9b6a45f68cd3c0e1deae96de52b2a5586
Davide Galli <86418153+d-galli@users.noreply.github.com>
2024-09-26 15:50:42 +0200

    Add files via upload
    Added two python files to test video stream over MQTT with Basler and IDS cameras.

---
+++ src/basler_test.py
@@ -0,0 +1,92 @@
+####################################################### LIBRARIES #######################################################
+import cv2
+import keyboard
+import paho.mqtt.client as mqtt
+import base64
+import time
+import numpy as np
+from pypylon import pylon
+
+####################################################### VARIABLES #######################################################
+
+# MQTT
+MQTT_BROKER = "localhost" # IP address of the MQTTT broker
+MQTT_PORT = 1883 # Port of the MQTT Broker
+MQTT_TOPIC = "optisort/basler/stream" # Topic on which frame will be published
+
+# OpenCV
+encode_param_png = [cv2.IMWRITE_PNG_COMPRESSION, 0]
+encode_param_jpg = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+########################################### USER-DEFINED FUNCTIONS ######################################################
+
+
+################################################## MAIN PROGRAM #########################################################
+
+print("Connecting to MQTT broker")
+client = mqtt.Client() # Create the MQTT Client
+client.connect(MQTT_BROKER, MQTT_PORT) # Establishing Connection with the Broker
+print("Connected to MQTT broker")
+
+print("Connecting to the Basler camera")
+
+# Conecting to the first Basler available camera
+baslerCamera = pylon.InstantCamera(pylon.TlFactory.GetInstance().CreateFirstDevice())
+
+# Grabing Continusely (video) with minimal delay
+baslerCamera.StartGrabbing(pylon.GrabStrategy_LatestImageOnly) 
+baslerConverter = pylon.ImageFormatConverter()
+
+# Converting to opencv bgr format
+baslerConverter.OutputPixelFormat = pylon.PixelType_BGR8packed
+baslerConverter.OutputBitAlignment = pylon.OutputBitAlignment_MsbAligned
+
+if not baslerCamera.IsGrabbing():
+    print("Error: Could not open camera.")
+print("Connected to Basler camera")
+
+print("Start streaming")
+print("Press 'Q' to quit")
+
+while True:
+
+    start = time.time() # Start time
+
+    grabResult = baslerCamera.RetrieveResult(5000, pylon.TimeoutHandling_ThrowException)
+
+    if grabResult.GrabSucceeded():
+        # Access the image data
+        image = baslerConverter.Convert(grabResult)
+        img = image.GetArray()
+        
+        # Create an OpenCV frame
+        frame = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        
+        # Resize the image by a half
+        frame = cv2.resize(frame,(0,0),fx=0.5, fy=0.5)
+  
+    grabResult.Release()
+    
+
+    # Encode image to PNG format
+    _frame = cv2.imencode('.png', frame, encode_param_png)[1].tobytes()
+    client.publish(MQTT_TOPIC, _frame) # Publish the Frame on the Topic home/server
+    cv2.imshow("Basler Camera Stream", frame) # Show the frame
+    end = time.time() # End time
+    t = end - start
+    fps = 1/t
+
+    print("FPS Basler: ", np.round(fps, 0), end="\r") # Print the FPS
+
+    
+    # Press q if you want to end the loop
+    if cv2.waitKey(1) & keyboard.is_pressed('q'):
+        print("\nQuitting...")
+        break
+
+# Releasing the resource    
+baslerCamera.StopGrabbing()
+cv2.destroyAllWindows()
+
+client.disconnect()
+print("\nStopped streaming")
---
+++ src/ids_test.py
@@ -0,0 +1,168 @@
+####################################################### LIBRARIES #######################################################
+import cv2
+import keyboard
+import paho.mqtt.client as mqtt
+import base64
+import time
+import numpy as np
+from pyueye import ueye
+
+####################################################### VARIABLES #######################################################
+
+# MQTT
+MQTT_BROKER = "localhost" # IP address of the MQTTT broker
+MQTT_PORT = 1883 # Port of the MQTT Broker
+MQTT_TOPIC = "optisort/ids/stream" # Topic on which frame will be published
+
+# OpenCV
+encode_param_png = [cv2.IMWRITE_PNG_COMPRESSION, 0]
+encode_param_jpg = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+
+########################################### USER-DEFINED FUNCTIONS ######################################################
+
+
+################################################## MAIN PROGRAM #########################################################
+
+print("Connecting to MQTT broker")
+client = mqtt.Client() # Create the MQTT Client
+client.connect(MQTT_BROKER, MQTT_PORT) # Establishing Connection with the Broker
+print("Connected to MQTT broker")
+
+print("Conecting to the IDS camera")
+
+# Connect to the first available camera and get its infos
+hCam = ueye.HIDS(0)             
+sInfo = ueye.SENSORINFO()
+cInfo = ueye.CAMINFO()
+pcImageMemory = ueye.c_mem_p()
+MemID = ueye.int()
+rectAOI = ueye.IS_RECT()
+pitch = ueye.INT()
+nBitsPerPixel = ueye.INT(24)    #24: bits per pixel for color mode; take 8 bits per pixel for monochrome
+channels = 3                    #3: channels for color mode(RGB); take 1 channel for monochrome
+m_nColorMode = ueye.INT()		# Y8/RGB16/RGB24/REG32
+bytes_per_pixel = int(nBitsPerPixel / 8)
+
+# Starts the driver and establishes the connection to the camera
+nRet = ueye.is_InitCamera(hCam, None)
+if nRet != ueye.IS_SUCCESS:
+    print("is_InitCamera ERROR")
+
+# Reads out the data hard-coded in the non-volatile camera memory and writes it to the data structure that cInfo points to
+nRet = ueye.is_GetCameraInfo(hCam, cInfo)
+if nRet != ueye.IS_SUCCESS:
+    print("is_GetCameraInfo ERROR")
+
+# You can query additional information about the sensor type used in the camera
+nRet = ueye.is_GetSensorInfo(hCam, sInfo)
+if nRet != ueye.IS_SUCCESS:
+    print("is_GetSensorInfo ERROR")
+
+nRet = ueye.is_ResetToDefault( hCam)
+if nRet != ueye.IS_SUCCESS:
+    print("is_ResetToDefault ERROR")
+
+# Set display mode to DIB
+nRet = ueye.is_SetDisplayMode(hCam, ueye.IS_SET_DM_DIB)
+
+# Set the right color mode
+if int.from_bytes(sInfo.nColorMode.value, byteorder='big') == ueye.IS_COLORMODE_BAYER:
+    # setup the color depth to the current windows setting
+    ueye.is_GetColorDepth(hCam, nBitsPerPixel, m_nColorMode)
+    bytes_per_pixel = int(nBitsPerPixel / 8)
+
+elif int.from_bytes(sInfo.nColorMode.value, byteorder='big') == ueye.IS_COLORMODE_CBYCRY:
+    # for color camera models use RGB32 mode
+    m_nColorMode = ueye.IS_CM_BGRA8_PACKED
+    nBitsPerPixel = ueye.INT(32)
+    bytes_per_pixel = int(nBitsPerPixel / 8)
+
+elif int.from_bytes(sInfo.nColorMode.value, byteorder='big') == ueye.IS_COLORMODE_MONOCHROME:
+    # for color camera models use RGB32 mode
+    m_nColorMode = ueye.IS_CM_MONO8
+    nBitsPerPixel = ueye.INT(8)
+    bytes_per_pixel = int(nBitsPerPixel / 8)
+
+else:
+    # for monochrome camera models use Y8 mode
+    m_nColorMode = ueye.IS_CM_MONO8
+    nBitsPerPixel = ueye.INT(8)
+    bytes_per_pixel = int(nBitsPerPixel / 8)
+
+# Can be used to set the size and position of an "area of interest"(AOI) within an image
+nRet = ueye.is_AOI(hCam, ueye.IS_AOI_IMAGE_GET_AOI, rectAOI, ueye.sizeof(rectAOI))
+if nRet != ueye.IS_SUCCESS:
+    print("is_AOI ERROR")
+
+width = rectAOI.s32Width
+height = rectAOI.s32Height
+
+
+# Allocates an image memory for an image having its dimensions defined by width and height and its color depth defined by nBitsPerPixel
+nRet = ueye.is_AllocImageMem(hCam, width, height, nBitsPerPixel, pcImageMemory, MemID)
+if nRet != ueye.IS_SUCCESS:
+    print("is_AllocImageMem ERROR")
+else:
+    # Makes the specified image memory the active memory
+    nRet = ueye.is_SetImageMem(hCam, pcImageMemory, MemID)
+    if nRet != ueye.IS_SUCCESS:
+        print("is_SetImageMem ERROR")
+    else:
+        # Set the desired color mode
+        nRet = ueye.is_SetColorMode(hCam, m_nColorMode)
+
+# Activates the camera's live video mode (free run mode)
+nRet = ueye.is_CaptureVideo(hCam, ueye.IS_DONT_WAIT)
+if nRet != ueye.IS_SUCCESS:
+    print("is_CaptureVideo ERROR")
+
+# Enables the queue mode for existing image memory sequences
+nRet = ueye.is_InquireImageMem(hCam, pcImageMemory, MemID, width, height, nBitsPerPixel, pitch)
+if nRet != ueye.IS_SUCCESS:
+    print("is_InquireImageMem ERROR")
+
+print("Connected to IDS camera")
+
+print("Start streaming")
+
+print("Press 'Q' to quit")
+
+while(nRet == ueye.IS_SUCCESS):
+
+    start = time.time() # Start time
+
+    # In order to display the image in an OpenCV window we need to extract the data of our image memory
+    array = ueye.get_data(pcImageMemory, width, height, nBitsPerPixel, pitch, copy=False)
+
+    # Reshape it in an numpy array
+    frame = np.reshape(array,(height.value, width.value, bytes_per_pixel))
+        
+    # Resize the image by a half
+    frame = cv2.resize(frame,(0,0),fx=0.5, fy=0.5)
+
+    # Encode image to PNG format
+    _frame = cv2.imencode('.png', frame, encode_param_png)[1].tobytes()
+    client.publish(MQTT_TOPIC, _frame) # Publish the Frame on the Topic home/server
+    cv2.imshow("IDS Camera Stream", frame) # Show the frame
+    end = time.time() # End time
+    t = end - start
+    fps = 1/t
+
+    print("FPS IDS: ", np.round(fps, 0), end="\r") # Print the FPS
+
+    
+    # Press q if you want to end the loop
+    if cv2.waitKey(1) & keyboard.is_pressed('q'):
+        print("\nqQuitting...")
+        break
+
+# Releases an image memory that was allocated using is_AllocImageMem() and removes it from the driver management
+ueye.is_FreeImageMem(hCam, pcImageMemory, MemID)
+
+# Disables the hCam camera handle and releases the data structures and memory areas taken up by the uEye camera
+ueye.is_ExitCamera(hCam)
+
+cv2.destroyAllWindows()
+
+client.disconnect()
+print("\nStopped streaming")
