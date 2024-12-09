091d37d751cadad71b21284d191d6bdb919a3b9f
Dylan Morelato <Dylan.morelato@gmail.com>
2024-12-09 22:32:49 +0100

    Added splashscreen with simulated timer

--- src/OptiSort/HMI/Properties/Resources.Designer.cs
+++ src/OptiSort/HMI/Properties/Resources.Designer.cs
@@ -203,6 +203,16 @@ namespace OptiSort.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
+        internal static System.Drawing.Bitmap OptisortLogo {
+            get {
+                object obj = ResourceManager.GetObject("OptisortLogo", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
         internal static System.Drawing.Bitmap playDisabled_2x2_pptx {
             get {
                 object obj = ResourceManager.GetObject("playDisabled_2x2_pptx", resourceCulture);
