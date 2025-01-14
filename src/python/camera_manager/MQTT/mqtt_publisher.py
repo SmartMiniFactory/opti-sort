b6e4e701288bdf02d1f78194c6d816cf2f5b2936
Dylan Morelato <Dylan.morelato@gmail.com>
2025-01-14 23:43:59 +0100

    python "main" implements async to parallelize camera acquisition and streaming; cycle determinism improved

--- src/python/camera_manager/MQTT/mqtt_publisher.py
+++ src/python/camera_manager/MQTT/mqtt_publisher.py
@@ -1,5 +1,26 @@
 import paho.mqtt.client as mqtt
 
+
+def on_disconnect(client, userdata, rc):
+    print(f"Disconnected from broker. Reason: {rc}")
+
+
+def get_connect_error_message(rc):
+    error_messages = {
+        0: "Connection successful.",
+        1: "Incorrect protocol version.",
+        2: "Invalid client identifier.",
+        3: "Server unavailable.",
+        4: "Bad username or password.",
+        5: "Not authorized."
+    }
+    return error_messages.get(rc, "Unknown error.")
+
+
+def on_message(client, userdata, msg):
+    print(f"Received message on {msg.topic}: {msg.payload.decode()}")
+
+
 class MQTTClient:
     def __init__(self, broker, port, client_name, topics):
         self.broker = broker
@@ -10,8 +31,8 @@ class MQTTClient:
 
         # Set callbacks for connect, disconnect, and message
         self.client.on_connect = self.on_connect
-        self.client.on_disconnect = self.on_disconnect
-        self.client.on_message = self.on_message
+        self.client.on_disconnect = on_disconnect
+        self.client.on_message = on_message
 
     def on_connect(self, client, userdata, flags, rc):
         if rc == 0:
@@ -20,24 +41,7 @@ class MQTTClient:
             for topic in self.topics:
                 self.client.subscribe(topic)
         else:
-            print(f"Failed to connect with result code {rc}. Error description: {self.get_connect_error_message(rc)}")
-
-    def get_connect_error_message(self, rc):
-        error_messages = {
-            0: "Connection successful.",
-            1: "Incorrect protocol version.",
-            2: "Invalid client identifier.",
-            3: "Server unavailable.",
-            4: "Bad username or password.",
-            5: "Not authorized."
-        }
-        return error_messages.get(rc, "Unknown error.")
-
-    def on_disconnect(self, client, userdata, rc):
-        print(f"Disconnected from broker. Reason: {rc}")
-
-    def on_message(self, client, userdata, msg):
-        print(f"Received message on {msg.topic}: {msg.payload.decode()}")
+            print(f"Failed to connect with result code {rc}. Error description: {get_connect_error_message(rc)}")
 
     def connect(self):
         try:
@@ -48,7 +52,7 @@ class MQTTClient:
         except Exception as e:
             print(f"Error connecting to MQTT broker: {e}")
 
-    def publish(self, topic, message):
+    async def publish(self, topic, message):
         if self.client.is_connected():
             # Publish a message to the given topic
             self.client.publish(topic, message)
--- src/python/camera_manager/main.py
+++ src/python/camera_manager/main.py
@@ -1,22 +1,12 @@
-"""
-DESCRIPTION
-This file represents the final version merging all the tests done on other files
-This will be exported to a .exe and run on the machinery, controlled by and interacting with the HMI (C#)
-Overall, it merges:
-- three camera handling
-- calibrations
-- mqtt communication
-- feature recognition
-"""
+
 
 # -------------------------------------------------------------------------------------------------------------------
 
 import pathlib
 import xml.etree.ElementTree as ET
-
 import cv2
 import numpy as np
-
+import asyncio
 from MQTT.mqtt_publisher import MQTTClient
 from cameras.ids import Ids
 from cameras.basler import Basler
@@ -32,13 +22,7 @@ import base64
 # -------------------------------------------------------------------------------------------------------------------
 
 
-def im2json(imdata):
-    jstr = json.dumps({"image": base64.b64encode(imdata).decode('ascii'), "timestamp": datetime.datetime.now().isoformat()})
-    return jstr
-
-
 def parse_appconfig(file_path):
-
     # set defaults
     broker = '127.0.0.1'
     port = 1883
@@ -68,135 +52,146 @@ def parse_appconfig(file_path):
         return broker, port, camera_topics
 
 
-# -------------------------------------------------------------------------------------------------------------------
+def im2json(imdata):
+    jstr = json.dumps(
+        {"image": base64.b64encode(imdata).decode('ascii'), "timestamp": datetime.datetime.now().isoformat()})
+    return jstr
 
 
-# acquire testing mode
-testing = ''
-while testing != 'c' and testing != 'w':
-    testing = input("Do you want to use your webcam or real cameras? (w/c): ")
+async def main():
+    global testing, webcam, ids_camera, basler_camera, luxonis_camera, mqtt_client
 
-if testing == 'c':
-    testing = False
-else:
-    testing = True
-
-# acquire encoding mode
-encoding = ''
-while encoding != "jpg" and encoding != "png":
-    encoding = input("Do you want to jpg or png as encoding mode? (jpg/png): ")
-
-encode_param_png = [cv2.IMWRITE_PNG_COMPRESSION, 0]
-encode_param_jpg = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
-
-
-# Define important directories or files
-script_dir = pathlib.Path(__file__).parent.resolve()
-appconfig_file = (script_dir / "../../OptiSort/HMI/App.config").resolve()
-
-# Initialize MQTT
-mqtt_client_name = "camera_manager" # name of the client (unique) connecting to the broker
-mqtt_broker, mqtt_port, mqtt_stream_topics = parse_appconfig(appconfig_file)
-mqtt_client = MQTTClient(mqtt_broker, mqtt_port, mqtt_client_name, mqtt_stream_topics)
-mqtt_client.connect()
-
-# locate configuration files
-ids_configfile = (script_dir / "../../OptiSort/HMI/Config/camera_configuration.ini").resolve()
-basler_configfile = (script_dir / "../../OptiSort/HMI/Config/camera_config.pfs").resolve()
-luxonis_configfile = (script_dir / "../../OptiSort/HMI/Config/camera_configuration.ini").resolve()
-
-"""ids_config = {
-    "exposure": 10000,  # Microseconds
-    "gain": 1.5,
-    "resolution": (1280, 720),  # Width, Height
-    "pixel_format": "RGB8"
-}"""
-
-
-if testing:
-    print("Opening webcam...")
-    webcam = cv2.VideoCapture(0)
-
-else:
-    # Instance IDS camera
-    print("Initializing IDS camera...")
-    ids_camera = Ids(camera_id="ids", config_path=ids_configfile)
-    ids_camera.initialize()
-    # ids_camera.configure()
-    ids_camera.set_roi(0, 0, 1280, 1024)
-    ids_camera.acquisition_start()
-
-    # Instance Basler camera
-    print("Initializing Basler camera...")
-    basler_camera = Basler(camera_id="basler", config_path=basler_configfile)
-    basler_camera.initialize()
-    basler_camera.acquisition_start()
-
-    # Instance Luxonis camera
-    print("Initializing Luxonis camera...")
-    luxonis_camera = Luxonis(camera_id="luxonis", config_path=luxonis_configfile)
-    luxonis_camera.initialize()
-    luxonis_camera.acquisition_start()
-
-
-# Instance image processor class
-print("Initializing image processor...")
-processor = ImageProcessor()
-
-# -------------------------------------------------------------------------------------------------------------------
-
-
-print("Starting loop...")
-
-# Main loop
-while True:
-
-    start = time.time()  # Start time
-
-    # TODO: consider async techniques / think about synchronization; manual screenshots or videostream??
-
-    # Capture frames from each camera
     if testing:
-        _, webcam_frame = webcam.read()
-        cv2.imshow("Webcam frame", webcam_frame)
+        tasks = [
+            capture_and_publish(webcam, "optisort/ids/stream", mqtt_client),
+            capture_and_publish(webcam, "optisort/basler/stream", mqtt_client),
+            capture_and_publish(webcam, "optisort/luxonis/stream", mqtt_client),
+        ]
+    else:
+        tasks = [
+                capture_and_publish(ids_camera, "optisort/ids/stream", mqtt_client),
+                capture_and_publish(basler_camera, "optisort/basler/stream", mqtt_client),
+                capture_and_publish(luxonis_camera, "optisort/luxonis/stream", mqtt_client),
+        ]
+    await asyncio.gather(*tasks)
 
-        # encode to PNG or JPG and then convert to JSON to publish mqtt message
-        """expression_if_true if condition else expression_if_false"""
-        encoded_webcam_frame = cv2.imencode('.png', webcam_frame, encode_param_png if encoding == "png" else encode_param_jpg)[1].tobytes()
 
-        # comment out below to simulate camera malfunctioning
-        mqtt_client.publish("optisort/ids/stream", im2json(encoded_webcam_frame))
-        mqtt_client.publish("optisort/basler/stream", im2json(encoded_webcam_frame))
-        mqtt_client.publish("optisort/luxonis/stream", im2json(encoded_webcam_frame))
+async def capture_and_publish(camera, topic, client):
+    global testing, encoding, encode_param
 
+    while True:
+        start = time.time()  # Start time
+
+        if testing:
+            # Capture frame from the testing camera (webcam)
+            ret, frame = camera.read()
+            if not ret:
+                print("Failed to capture webcam frame.")
+                return False
+        else:
+            # Capture frames from the real cameras
+            frame = camera.capture_frame()
+
+        # show frame
+        cv2.imshow("Webcam frame", frame)
+
+        # Encode the frame
+        encoded_frame = cv2.imencode("." + encoding, frame, encode_param)[1].tobytes()
+
+        # Publish frame to MQTT topics
+        await client.publish(topic, im2json(encoded_frame))
+
+        # Handle frame display and quitting
+        if cv2.waitKey(1) & keyboard.is_pressed('q'):
+            print("\nQuitting...")
+            return False
+
+        # Calculate FPS and sleep for the remaining time
+        end = time.time()
+        elapsed = end - start
+        fps = 1 / elapsed
+        print("FPS: ", np.round(fps, 0), end="\r")
+
+        if await asyncio.sleep(0.2 - elapsed):  # Adjust interval based on elapsed time
+            return True
+
+# -------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------------------------------------
+
+if __name__ == "__main__":
+
+    # acquire testing mode
+    testing = ''
+    while testing != 'c' and testing != 'w':
+        testing = input("Do you want to use your webcam or real cameras? (w/c): ")
+
+    if testing == 'c':
+        testing = False
+    else:
+        testing = True
+
+    # acquire encoding mode
+    encoding = ''
+    while encoding != "jpg" and encoding != "png":
+        encoding = input("Do you want to jpg or png as encoding mode? (jpg/png): ")
+
+    # define encoding parameters
+    if encoding == "jpg":
+        encode_param = [int(cv2.IMWRITE_JPEG_QUALITY), 90]
+    else:
+        encode_param = [cv2.IMWRITE_PNG_COMPRESSION, 0]
+
+    # Define important directories or files
+    script_dir = pathlib.Path(__file__).parent.resolve()
+    appconfig_file = (script_dir / "../../OptiSort/HMI/App.config").resolve()
+
+    # Initialize MQTT
+    mqtt_client_name = "camera_manager"  # name of the client (unique) connecting to the broker
+    mqtt_broker, mqtt_port, mqtt_stream_topics = parse_appconfig(appconfig_file)
+    mqtt_client = MQTTClient(mqtt_broker, mqtt_port, mqtt_client_name, mqtt_stream_topics)
+    mqtt_client.connect()
+
+    # locate configuration files
+    ids_configfile = (script_dir / "../../OptiSort/HMI/Config/camera_configuration.ini").resolve()
+    basler_configfile = (script_dir / "../../OptiSort/HMI/Config/camera_config.pfs").resolve()
+    luxonis_configfile = (script_dir / "../../OptiSort/HMI/Config/camera_configuration.ini").resolve()
+
+    """ids_config = {
+        "exposure": 10000,  # Microseconds
+        "gain": 1.5,
+        "resolution": (1280, 720),  # Width, Height
+        "pixel_format": "RGB8"
+    }"""
+
+    if testing:
+        print("Opening webcam...")
+        webcam = cv2.VideoCapture(0)
 
     else:
-        ids_frame = ids_camera.capture_frame()
-        basler_frame = basler_camera.capture_frame()
-        luxonis_frame = luxonis_camera.capture_frame()
+        # Instance IDS camera
+        print("Initializing IDS camera...")
+        ids_camera = Ids(camera_id="ids", config_path=ids_configfile)
+        ids_camera.initialize()
+        # ids_camera.configure()
+        ids_camera.set_roi(0, 0, 1280, 1024)
+        ids_camera.acquisition_start()
 
-        cv2.imshow("IDS frame", ids_frame)
-        cv2.imshow("Basler frame", basler_frame)
-        cv2.imshow("Luxonis frame", luxonis_frame)
+        # Instance Basler camera
+        print("Initializing Basler camera...")
+        basler_camera = Basler(camera_id="basler", config_path=basler_configfile)
+        basler_camera.initialize()
+        basler_camera.acquisition_start()
 
-        # encode to PNG or JPG and then convert to JSON to publish mqtt message
-        encoded_ids_frame = cv2.imencode('.png', ids_frame, encode_param_png if encoding == "png" else encode_param_jpg)[1].tobytes()
-        encoded_basler_frame = cv2.imencode('.png', basler_frame, encode_param_png if encoding == "png" else encode_param_jpg)[1].tobytes()
-        encoded_luxonis_frame = cv2.imencode('.png', luxonis_frame, encode_param_png if encoding == "png" else encode_param_jpg)[1].tobytes()
+        # Instance Luxonis camera
+        print("Initializing Luxonis camera...")
+        luxonis_camera = Luxonis(camera_id="luxonis", config_path=luxonis_configfile)
+        luxonis_camera.initialize()
+        luxonis_camera.acquisition_start()
 
-        mqtt_client.publish("optisort/ids/stream", im2json(encoded_ids_frame))
-        mqtt_client.publish("optisort/basler/stream", im2json(encoded_basler_frame))
-        mqtt_client.publish("optisort/luxonis/stream", im2json(encoded_luxonis_frame))
+    # Instance image processor class
+    print("Initializing image processor...")
+    processor = ImageProcessor()
 
-
-    # Press q if you want to end the loop
-    if cv2.waitKey(1) & keyboard.is_pressed('q'):
-        print("\nqQuitting...")
-        break
-
-    end = time.time()  # End time
-    t = end - start
-    fps = 1 / t
-    print("FPS: ", np.round(fps, 0), end="\r")  # Print the FPS\"""
-
-    time.sleep(0.2)  # streaming performance depends also on publishing frequency; 1/15 (FPS) = 0.66
+    print("Starting loop...")
+    asyncio.run(main())
